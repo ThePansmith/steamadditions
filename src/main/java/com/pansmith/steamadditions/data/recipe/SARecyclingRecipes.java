0d4ce6d0d8c9cded7827259c426821b9f6647cca
AncientShotgun <leon.pratt03@gmail.com>
2024-09-14 17:01:13 +0000

    Update SARecyclingRecipes.java
    Corrected class imports.

--- src/main/java/com/pansmith/steamadditions/data/recipe/SARecyclingRecipes.java
+++ src/main/java/com/pansmith/steamadditions/data/recipe/SARecyclingRecipes.java
@@ -1,14 +1,13 @@
-import com.gregtechceu.gtceu.data.recipe.misc.RecyclingRceipes;
-import com.gregtechceu.gtceu.api.data.chemical.ChemicalHelper;
-import com.gregtechceu.gtceu.api.data.chemical.material.stack.ItemMaterialInfo;
+import com.gregtechceu.gtceu.data.recipe.misc.RecyclingRecipes;
 import com.gregtechceu.gtceu.common.data.GTMaterials;
+import com.gregtechceu.gtceu.api.data.chemical.material.stack.MaterialStack;
 
 import com.pansmith.steamadditions.common.data.SAMachines;
 
 public class SARecyclingRecipes {
 
   public static void init(Consumer<FinishedRecipe> provider) {
-    ArrayList<MaterialStack> materialStacks = new ArrayList<>(new MaterialStack(GTMaterials.Bronze),new MaterialStack(GTMaterials.Brick));
+    ArrayList<MaterialStack> materialStacks = new ArrayList<>(new MaterialStack(GTMaterials.Bronze), new MaterialStack(GTMaterials.Brick));
     RecyclingRecipes.registerRecyclingRecipes‎(provider, SAMachines.STEAM_CENTRIFUGE.asStack(), materialStacks, false, null);
     RecyclingRecipes.registerRecyclingRecipes‎(provider, SAMachines.STEAM_ALLOY_SMELTER.asStack(), materialStacks, false, null);
   }
