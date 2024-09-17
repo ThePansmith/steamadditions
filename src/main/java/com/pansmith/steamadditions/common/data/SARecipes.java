6c53242802027b0c807fa577fd37fdd142a4cf82
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2024-09-17 15:47:00 -0500

    Needs to be updated to 1.4.1 i think lole
    This reverts commit f67f72a8820ef067580a61f974a3de787d2c0fc2, reversing
    changes made to 8dbf5c46585dc5281952ebbe95aadbae919dc91c.

--- src/main/java/com/pansmith/steamadditions/common/data/SARecipes.java
+++ src/main/java/com/pansmith/steamadditions/common/data/SARecipes.java
@@ -3,7 +3,6 @@ package com.pansmith.steamadditions.common.data;
 import com.pansmith.steamadditions.data.recipe.*;
 import net.minecraft.data.recipes.FinishedRecipe;
 import com.pansmith.steamadditions.data.recipe.MiscRecipes;
-import com.pansmith.steamadditions.data.recipe.SARecyclingRecipes;
 
 import java.util.function.Consumer;
 
@@ -11,6 +10,5 @@ public class SARecipes {
 
     public static void init(Consumer<FinishedRecipe> provider) {
         MiscRecipes.init(provider);
-        SARecyclingRecipes.init(provider);
     }
 }
